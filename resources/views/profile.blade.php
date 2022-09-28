36097e255106fd3e04434dfa5c6c323326b4d020
AceJoyGwapo <aceejoygwapo123@gmail.com>
2022-09-28 15:40:29 +0800

    First Commit

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Ace Joy Prelim</title>
+    <script src="{{asset('js/bootstrap.min.js')}}"></script>
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+</head>
+<body>
+    <div class="text-center">
+        <nav class="nav justify-content-center">
+            <a href="{{url('/')}}" class="nav-link">Home</a>
+            <a href="{{url('/about')}}" class="nav-link">About Us</a>
+            <a href="{{url('/profile')}}" class="nav-link">Company Profile</a>
+        </nav>
+        <h1>Company Profile</h1>
+
+        <div class="card shadow-sm mx-auto" style="width: 500px">
+            <div class="card-body">
+                <div class="card-title">Company Profile</div>
+                <p>
+                    Island City Mall has redefined the concept of shopping, dining, and entertainment in Bohol upon its... Dampas District, 6300 Tagbilaran City, Philippines.
+                </p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
